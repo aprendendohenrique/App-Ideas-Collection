2b3874c17dc101af75f23a0590d8fab596d7403a
aprendendohenrique <henriquericardofodi@gmail.com>
2026-07-09 18:20:34 -0300

    Learning Tinkers/customtkinter continue

--- Beginner/JSON2CSV/app.py
+++ Beginner/JSON2CSV/app.py
@@ -1,27 +1,50 @@
+import os
+
 import customtkinter
 
 
+class MyCheckboxFrame(customtkinter.CTkFrame):
+    """Frame class for the checkboxes"""
+
+    def __init__(self, master, values):
+        super().__init__(master)
+        self.checkboxes = []
+
+        for i, value in enumerate(values):
+            checkbox = customtkinter.CTkCheckBox(self, text=value)
+            checkbox.grid(row=i, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.checkboxes.append(checkbox)
+
+    def get(self):
+        checked_checkboxes = []
+        for checkbox in self.checkboxes:
+            if checkbox.get() == 1:
+                checked_checkboxes.append(checkbox.cget("text"))
+        return checked_checkboxes
+
+
 class App(customtkinter.CTk):
-    """Class App"""
+    """App Class"""
+
     def __init__(self):
         super().__init__()
 
         self.title("my app")
         self.geometry("400x180")
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure((0, 1), weight=1)
+        self.grid_columnconfigure((0, 1), weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
-        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox_1")
-        self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
-
-        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox_2")
-        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.checkbox_frame_1 = MyCheckboxFrame(self, values=["value_1", "value_2", "value_3"])
+        self.checkbox_frame_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
+        self.checkbox_frame_2 = MyCheckboxFrame(self, values=["option_1", "option_2"])
+        self.checkbox_frame_2.grid(row=0, column=1, padx=(0, 10), pady=(10, 0), sticky="nsew")
 
         self.button = customtkinter.CTkButton(self, text="My Button", command=self.button_callback)
         self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
 
     def button_callback(self):
-        print("Button pressed")
+        print("checked checkboxes: ", self.checkbox_frame_1.get())
+        print("checked checkboxes: ", self.checkbox_frame_2.get())
 
 app = App()
 app.mainloop()
